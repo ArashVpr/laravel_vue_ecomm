593c3ed2576dc444f2af2646e0689b2de89d057f
Arash <avafapour@innovqube.com>
2025-06-04 15:14:16 +0200

    apply auth and guest

--- vuejs_admin/src/router/index.js
+++ vuejs_admin/src/router/index.js
@@ -5,12 +5,16 @@ import Login from '../views/Login.vue'
 import ResetPassword from '../views/ResetPassword.vue'
 import RequestResetPassword from '../views/RequestResetPassword.vue'
 import AppLayout from '../components/AppLayout.vue'
+import store from '../store'
 
 const routes = [
     {
         path: '/app',
         name: 'app',
         component: AppLayout,
+        meta: {
+            requiresAuth: true
+        },
         children: [
             {
                 path: 'dashboard',
@@ -29,7 +33,7 @@ const routes = [
     {
         path: '/login',
         name: 'login',
-        component: Login
+        component: Login,
     },
     {
         path: '/request-password',
@@ -46,7 +50,22 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes
+  })
+
+router.beforeEach((to, from, next) => {
+
+if (to.meta.requiresAuth && !store.state.user.token) {
+    next({ name: 'login' })
+} else if (!to.meta.requiresAuth && store.state.user.token) {
+    next({ name: 'app.dashboard' })
+}
+else {
+    next()
+}
+
 })
+
+
 
 export default router
 
